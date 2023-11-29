54c625af6a46ff221f8b95d437b96cd5e6ebd8a0
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2023-11-29 13:19:30 -0800

    neovim: formatting

--- homeroot/.config/nvim/init.lua
+++ homeroot/.config/nvim/init.lua
@@ -4,65 +4,63 @@ vim.g.loaded_netrwPlugin = 1
 
 --Pluginload
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lualine/lualine.nvim'
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lualine/lualine.nvim'
 
-  use {'nvim-tree/nvim-tree.lua'}
-  use {'romgrk/barbar.nvim'}
-  use { "ellisonleao/gruvbox.nvim"}
-  use {"akinsho/toggleterm.nvim", tag = '*'}
+    use { 'nvim-tree/nvim-tree.lua' }
+    use { 'romgrk/barbar.nvim' }
+    use { "ellisonleao/gruvbox.nvim" }
+    use { "akinsho/toggleterm.nvim", tag = '*' }
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
     }
-  }
 
-  use {'lewis6991/gitsigns.nvim'}
-  use {'mg979/vim-visual-multi'}
-  if packer_bootstrap then
-    require('packer').sync()
-
-  end
-
+    use { 'lewis6991/gitsigns.nvim' }
+    use { 'mg979/vim-visual-multi' }
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
 --BUFFERLINE
-require("bufferline").setup{
- icons = {
-     button = 'X',
-     filetype = { 
-         enabled = false,
+require("bufferline").setup {
+    icons = {
+        button = 'X',
+        filetype = {
+            enabled = false,
         },
         pinned = {
             button = '<-', filename = true
@@ -72,105 +70,111 @@ require("bufferline").setup{
 
 --COMPLETE
 local cmp = require('cmp')
- cmp.setup({
+cmp.setup({
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true}),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
-        sources = cmp.config.sources({
-      { name = 'nvim_lsp' }
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' }
     }, {
-      { name = 'buffer' }
+        { name = 'buffer' }
     })
 })
 
 --GITSIGNS
 require('gitsigns').setup()
 
---LSP
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+--LSP (autofmt)
+local lsp = require('lsp-zero')
+lsp.preset({
+    name = 'minimal',
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+    suggest_lsp_servers = false,
 })
 lsp.setup()
 
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
+end)
+
 --NVIM-TREE
 require("nvim-tree").setup({
-respect_buf_cwd = false,
-update_focused_file = {
-enable = true,
-update_root = true,
-},
-respect_buf_cwd=true,
-renderer = {
+    respect_buf_cwd = false,
+    update_focused_file = {
+        enable = true,
+        update_root = true,
+    },
+    respect_buf_cwd = true,
+    renderer = {
         icons = {
-          glyphs = {
-            default = "FL",
-            symlink = "SYM",
-            bookmark = "BK",
-            modified = "M",
-            folder = {
-              arrow_closed = ">",
-              arrow_open = "^",
-              default = "FLR",
-              open = "FLR(O)",
-              empty = "FLR(E)",
-              empty_open = "FLR(O,E)",
-              symlink = "SYM",
-              symlink_open = "SYM(O)",
+            glyphs = {
+                default = "FL",
+                symlink = "SYM",
+                bookmark = "BK",
+                modified = "M",
+                folder = {
+                    arrow_closed = ">",
+                    arrow_open = "^",
+                    default = "FLR",
+                    open = "FLR(O)",
+                    empty = "FLR(E)",
+                    empty_open = "FLR(O,E)",
+                    symlink = "SYM",
+                    symlink_open = "SYM(O)",
+                },
+                git = {
+                    unstaged = "X",
+                    staged = "V",
+                    unmerged = "UM",
+                    renamed = "RN",
+                    untracked = "UT",
+                    deleted = "DEL",
+                    ignored = "IGN",
+                },
             },
-	git = {
-              unstaged = "X",
-              staged = "V",
-              unmerged = "UM",
-              renamed = "RN",
-              untracked = "UT",
-              deleted = "DEL",
-              ignored = "IGN",
+            show = {
+                file = false,
             },
-          },
-	  show={
-	     file=false,
-	  },
         },
-        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md","go.mod","go.sum","build.sh" },
+        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "go.mod", "go.sum", "build.sh" },
         symlink_destination = true,
-      },
+    },
 })
 
 local nvim_tree_events = require('nvim-tree.events')
 local bufferline_api = require('bufferline.api')
 
 local function get_tree_size()
-  return require'nvim-tree.view'.View.width
+    return require 'nvim-tree.view'.View.width
 end
 
 nvim_tree_events.subscribe('TreeOpen', function()
-  bufferline_api.set_offset(get_tree_size())
+    bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe('Resize', function()
-  bufferline_api.set_offset(get_tree_size())
+    bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe('TreeClose', function()
-  bufferline_api.set_offset(0)
+    bufferline_api.set_offset(0)
 end)
 
 --TOGGLETERM
 require("toggleterm").setup()
 
 --Keymaps
-vim.keymap.set({"n"},"<tab>","<Cmd>bnext<CR>")
-vim.keymap.set({"n"},"<c-w>","<Cmd>NvimTreeToggle<CR>")
+vim.keymap.set({ "n" }, "<tab>", "<Cmd>bnext<CR>")
+vim.keymap.set({ "n" }, "<c-w>", "<Cmd>NvimTreeToggle<CR>")
 --Preferences
 vim.opt.termguicolors = true
-vim.cmd([[colorscheme gruvbox]]) 
+vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[set number]])
 vim.cmd([[set clipboard=unnamedplus]])
 vim.cmd([[set tabstop=4]])
@@ -178,7 +182,6 @@ vim.cmd([[set shiftwidth=4]])
 vim.cmd([[set expandtab]])
 
 --Commands
-vim.api.nvim_create_user_command("Nvimreload",function()
-  vim.cmd([[source ~/.config/nvim/init.lua]])
-end,{})
-
+vim.api.nvim_create_user_command("Nvimreload", function()
+    vim.cmd([[source ~/.config/nvim/init.lua]])
+end, {})

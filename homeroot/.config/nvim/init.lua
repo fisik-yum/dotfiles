49bb78dcacf3e950c56f9e1e302d6c82c59fbe55
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2023-12-08 17:02:00 -0800

    other stuff

--- homeroot/.config/nvim/init.lua
+++ homeroot/.config/nvim/init.lua
@@ -67,9 +67,9 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' }
-    }, {
+    }--[[, {
         { name = 'buffer' }
-    })
+    }]])
 })
 
 --GITSIGNS
@@ -134,25 +134,6 @@ require("nvim-tree").setup({
     },
 })
 
---[[local nvim_tree_events = require('nvim-tree.events')
-local bufferline_api = require('bufferline.api')
-
-local function get_tree_size()
-    return require 'nvim-tree.view'.View.width
-end
-
-nvim_tree_events.subscribe('TreeOpen', function()
-    bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('Resize', function()
-    bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('TreeClose', function()
-    bufferline_api.set_offset(0)
-end)
-]]
 --TOGGLETERM
 require("toggleterm").setup()
 

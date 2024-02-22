521ccf665c7b66d6e9c6a060739343031e9bed66
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2024-02-22 08:45:45 -0800

    migrate from packer to pckr

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,57 +3,58 @@ vim.g.loaded_netrwPlugin = 1
 
 
 --Pluginload
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
+local function bootstrap_pckr()
+    local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+    if not vim.loop.fs_stat(pckr_path) then
+        vim.fn.system({
+            'git',
+            'clone',
+            "--filter=blob:none",
+            'https://github.com/lewis6991/pckr.nvim',
+            pckr_path
+        })
     end
-    return false
+
+    vim.opt.rtp:prepend(pckr_path)
 end
 
-local packer_bootstrap = ensure_packer()
+bootstrap_pckr()
 
-require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'nvim-lualine/lualine.nvim'
+require('pckr').add {
+    'wbthomason/packer.nvim',
+    'nvim-lualine/lualine.nvim',
+    'nvim-tree/nvim-tree.lua',
+    'ellisonleao/gruvbox.nvim',
+    'akinsho/toggleterm.nvim',
 
-    use { 'nvim-tree/nvim-tree.lua' }
-    use { "ellisonleao/gruvbox.nvim" }
-    use { "akinsho/toggleterm.nvim", tag = '*' }
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
+    { 'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
+            'neovim/nvim-lspconfig',               -- Required
+            'williamboman/mason.nvim',             -- Optional
+            'williamboman/mason-lspconfig.nvim',   -- Optional
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },       -- Optional
-            { 'hrsh7th/cmp-path' },         -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+            'hrsh7th/nvim-cmp',                    -- Required
+            'hrsh7th/cmp-nvim-lsp',                -- Required
+            'hrsh7th/cmp-buffer',                  -- Optional
+            'hrsh7th/cmp-path',                    -- Optional
+            'saadparwaiz1/cmp_luasnip',            -- Optional
+            'hrsh7th/cmp-nvim-lua',                -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },             -- Required
-            { 'rafamadriz/friendly-snippets' }, -- Optional
-        }
-    }
+            'L3MON4D3/LuaSnip',               -- Required
+            'rafamadriz/friendly-snippets',   -- Optional
+        },
+    },
 
-    use { 'lewis6991/gitsigns.nvim' }
-    use { 'mg979/vim-visual-multi' }
-    use { 'm4xshen/autoclose.nvim' }
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+
+    'lewis6991/gitsigns.nvim',
+    'mg979/vim-visual-multi',
+    'm4xshen/autoclose.nvim',
+
+}
 
 --COMPLETE
 local cmp = require('cmp')

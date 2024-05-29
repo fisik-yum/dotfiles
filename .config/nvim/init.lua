e5359b286b0b9b9b1d33c1075b02f3dee5f9159c
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2024-05-29 10:35:25 -0700

    fix nvim-cmp problems

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,12 +33,16 @@ require('lazy').setup({
             'williamboman/mason.nvim',           -- Optional
             'williamboman/mason-lspconfig.nvim', -- Optional
             -- Autocompletion
-            'hrsh7th/nvim-cmp',                  -- Required
-            'hrsh7th/cmp-nvim-lsp',              -- Required
-            'hrsh7th/cmp-buffer',                -- Optional
-            'hrsh7th/cmp-path',                  -- Optional
-            'saadparwaiz1/cmp_luasnip',          -- Optional
-            'hrsh7th/cmp-nvim-lua',              -- Optional
+            {
+                'hrsh7th/nvim-cmp',
+                commit = 'b356f2c',
+                pin = true,
+            },                          -- Required
+            'hrsh7th/cmp-nvim-lsp',     -- Required
+            'hrsh7th/cmp-buffer',       -- Optional
+            'hrsh7th/cmp-path',         -- Optional
+            'saadparwaiz1/cmp_luasnip', -- Optional
+            'hrsh7th/cmp-nvim-lua',     -- Optional
 
             -- Snippets
             'L3MON4D3/LuaSnip',             -- Required

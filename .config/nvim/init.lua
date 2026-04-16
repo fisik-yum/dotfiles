a910630906b60e18e1d0123873daadc7e050d99a
fisik-yum <54129197+fisik-yum@users.noreply.github.com>
2026-04-15 18:30:49 -0700

    NeoVim 0.12 tree-sitter changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,124 +3,118 @@ vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-    --    'nvim-lualine/lualine.nvim',
-    'nvim-tree/nvim-tree.lua',
-    'navarasu/onedark.nvim',
-    'akinsho/toggleterm.nvim',
+require("lazy").setup({
+	--    'nvim-lualine/lualine.nvim',
+	"nvim-tree/nvim-tree.lua",
+	"navarasu/onedark.nvim",
+	"akinsho/toggleterm.nvim",
 
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'L3MON4D3/LuaSnip',
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"L3MON4D3/LuaSnip",
 
-    'mg979/vim-visual-multi',
-
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    {
-        'saghen/blink.cmp',
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        version = '1.*',
-    },
-    "airblade/vim-gitgutter",
+	"mg979/vim-visual-multi",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		branch = "main",
+	},
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "1.*",
+	},
+	"airblade/vim-gitgutter",
+	"barrettruth/live-server.nvim",
 })
 
---TREESITTER
-require 'nvim-treesitter.configs'.setup {
-
-    highlight = {
-        enable = true,
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-
-        additional_vim_regex_highlighting = { "python" },
-        indent = {enable = false}
-    },
-}
-
+require("nvim-treesitter").setup({
+	install_dir = vim.fn.stdpath("data") .. "/site",
+})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go", "gomod", "lua", "rust", "gotempl", "markdown" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
 --MASON
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {},
-    handlers = {
-        -- this first function is the "default handler"
-        -- it applies to every language server without a custom handler
-        function(server_name)
-            vim.lsp.config[server_name].setup({})
-        end,
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {},
+	handlers = {
+		-- this first function is the "default handler"
+		-- it applies to every language server without a custom handler
+		function(server_name)
+			vim.lsp.config[server_name].setup({})
+		end,
 
-        -- this is the "custom handler" for `lua_ls`
-        lua_ls = function()
-            require('lspconfig').lua_ls.setup({
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            globals = { 'vim' },
-                        },
-                        workspace = {
-                            library = { vim.env.VIMRUNTIME },
-                        },
-                    },
-                },
-            })
-        end,
-    },
+		-- this is the "custom handler" for `lua_ls`
+		lua_ls = function()
+			require("lspconfig").lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = { vim.env.VIMRUNTIME },
+						},
+					},
+				},
+			})
+		end,
+	},
 })
 
 -- LSP
-vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
-    callback = function(event)
-        local opts = { buffer = event.buf }
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'E', '<cmd>lua vim.diagnostic.open_float()<CR>')
-        vim.keymap.set('n', '<Leader>f', '<cmd>lua vim.lsp.buf.format()<CR>')
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end
+		vim.keymap.set("n", "<Leader>k", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "<Leader>f", "<cmd>lua vim.lsp.buf.format()<CR>")
+		vim.keymap.set("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+		vim.keymap.set("n", "<Leader>R", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set("n", "<Leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+	end,
 })
 
 require("blink.cmp").setup({
-    keymap = { preset = "default" },
-    fuzzy = { implementation = "prefer_rust" },
-    completion = {
-        documentation = {
-            auto_show = true,
-        },
-    },
-    sources = {
-        -- `lsp`, `buffer`, `snippets`, `path` and `omni` are built-in
-        -- so you don't need to define them in `sources.providers`
-        default = { 'lsp', 'snippets', 'path' },
-    }
-
+	keymap = { preset = "default" },
+	fuzzy = { implementation = "prefer_rust" },
+	completion = {
+		documentation = {
+			auto_show = true,
+		},
+	},
+	sources = {
+		-- `lsp`, `buffer`, `snippets`, `path` and `omni` are built-in
+		-- so you don't need to define them in `sources.providers`
+		default = { "lsp", "snippets", "path" },
+	},
 })
 
 --require('lualine').setup {
@@ -130,51 +124,50 @@ require("blink.cmp").setup({
 --    }
 --}
 
-
 --NVIM-TREE
 require("nvim-tree").setup({
-    update_focused_file = {
-        enable = true,
-        update_root = true,
-    },
-    respect_buf_cwd = true,
-    renderer = {
-        icons = {
-            glyphs = {
-                default = "FL",
-                symlink = "SYM",
-                bookmark = "BK",
-                modified = "M",
-                folder = {
-                    arrow_closed = ">",
-                    arrow_open = "^",
-                    default = "FLR",
-                    open = "FLR(O)",
-                    empty = "FLR(E)",
-                    empty_open = "FLR(O,E)",
-                    symlink = "SYM",
-                    symlink_open = "SYM(O)",
-                },
-                git = {
-                    unstaged = "X",
-                    staged = "V",
-                    unmerged = "UM",
-                    renamed = "RN",
-                    untracked = "UT",
-                    deleted = "DEL",
-                    ignored = "IGN",
-                },
-            },
-            show = {
-                file = false,
-            },
-        },
-        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "go.mod", "go.sum", "build.sh" },
-        symlink_destination = true,
-    },
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
+	respect_buf_cwd = true,
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "FL",
+				symlink = "SYM",
+				bookmark = "BK",
+				modified = "M",
+				folder = {
+					arrow_closed = ">",
+					arrow_open = "^",
+					default = "FLR",
+					open = "FLR(O)",
+					empty = "FLR(E)",
+					empty_open = "FLR(O,E)",
+					symlink = "SYM",
+					symlink_open = "SYM(O)",
+				},
+				git = {
+					unstaged = "X",
+					staged = "V",
+					unmerged = "UM",
+					renamed = "RN",
+					untracked = "UT",
+					deleted = "DEL",
+					ignored = "IGN",
+				},
+			},
+			show = {
+				file = false,
+			},
+		},
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "go.mod", "go.sum", "build.sh" },
+		symlink_destination = true,
+	},
 })
 --TOGGLETERM
-require("toggleterm").setup()
+-- require("toggleterm").setup()
 
 --Keymaps
 vim.g.mapleader = " "
@@ -197,11 +190,11 @@ vim.cmd([[set relativenumber]])
 --vim.cmd([[highlight ColorColumn ctermbg=0 guibg=lavender]])
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = false,
-    float = true,
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = false,
+	float = true,
 })
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
